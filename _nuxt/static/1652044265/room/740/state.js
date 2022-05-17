6f632653ad9961c5232aa34f581928aef6d4dac3
zabastx <zabastx@users.noreply.github.com>
2022-05-17 03:36:01 +0000

    deploy: 496a1d762dafb7bf8b57c27427911686641163db

--- _nuxt/static/1652044265/room/740/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-hotel\u002F_nuxt\u002Fstatic\u002F1652044265",layout:"default",error:a,serverRendered:true,routePath:"\u002Froom\u002F740",config:{_app:{basePath:"\u002Fnuxt-hotel\u002F",assetsPath:"\u002Fnuxt-hotel\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cb37ea62"}}}}(null));
---
+++ _nuxt/static/1652758554/room/888/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-hotel\u002F_nuxt\u002Fstatic\u002F1652758554",layout:"default",error:a,serverRendered:true,routePath:"\u002Froom\u002F888",config:{_app:{basePath:"\u002Fnuxt-hotel\u002F",assetsPath:"\u002Fnuxt-hotel\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2f267feb"}}}}(null));
