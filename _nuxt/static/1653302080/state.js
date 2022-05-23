3f8451e9a4d0ea17e88b8b076b05e093968203d8
zabastx <zabastx@users.noreply.github.com>
2022-05-23 10:34:48 +0000

    deploy: 50dfba3c4180e79525204d97f79bf6fe539a404f

---
+++ _nuxt/static/1653302080/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-hotel\u002F_nuxt\u002Fstatic\u002F1653302080",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-hotel\u002F",assetsPath:"\u002Fnuxt-hotel\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5ce993da"}}}}(null));
