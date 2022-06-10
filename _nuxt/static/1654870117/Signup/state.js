31beefc72b38a444aa1ab220f731efc0012eee51
zabastx <zabastx@users.noreply.github.com>
2022-06-10 14:08:44 +0000

    deploy: 4b4ecb3dd8d8d86ce96fa25e377177ffab0ba3a6

---
+++ _nuxt/static/1654870117/Signup/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-hotel\u002F_nuxt\u002Fstatic\u002F1654870117",layout:"default",error:a,serverRendered:true,routePath:"\u002FSignup",config:{_app:{basePath:"\u002Fnuxt-hotel\u002F",assetsPath:"\u002Fnuxt-hotel\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0ddf14bc"}}}}(null));
