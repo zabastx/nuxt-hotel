2f6966011c8b27bdcc0e35d9313ff8d98a59677d
zabastx <zabastx@users.noreply.github.com>
2022-10-19 07:02:34 +0000

    deploy: dadbb245fd8bacb25fc42867b5549287d99debce

--- _nuxt/static/1666162922/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FLogin","\u002FSignup","\u002Flogin","\u002Fsignup"]})
---
+++ _nuxt/static/1666162951/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002FLogin","\u002F","\u002FSignup","\u002Flogin","\u002Fsignup"]})
